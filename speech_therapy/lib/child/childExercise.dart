8eeb51527e235e948eb1968d4a83361de20849d1
Mousa Tams <mousatams@hotmail.com>
2024-08-12 00:01:07 +0300

    modified:   .gitignore prevent api exposure 	modified:   speech_therapy/lib/child/childExercise.dart added g2p, gemini, and text to speech api

--- speech_therapy/lib/child/childExercise.dart
+++ speech_therapy/lib/child/childExercise.dart
@@ -1,12 +1,16 @@
-import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_therapy/googleCloudAPIs/g2p_api.dart';
+import 'package:speech_therapy/googleCloudAPIs/gemini_api.dart';
+import 'package:speech_therapy/googleCloudAPIs/text_to_speech_api.dart';
 import 'package:video_player/video_player.dart';
 import 'package:chewie/chewie.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:firebase_database/firebase_database.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class CustomCacheManager {
   static final CacheManager _cacheManager = CacheManager(
@@ -44,10 +48,13 @@ class _VideoPlaybackPageState extends State<VideoPlaybackPage> {
   late VideoPlayerController _controller;
   late ChewieController _chewieController;
   late stt.SpeechToText _speech;
+  final AudioPlayer _audioPlayer = AudioPlayer();
   bool _isListening = false;
   bool _isLoading = true;
   String _recognizedText = '';
   bool _showCelebration = false;
+  bool aboveSimilarityThreshhold = false;
+  bool _useIPA = true; // Toggle for using IPA
 
   @override
   void initState() {
@@ -110,41 +117,134 @@ class _VideoPlaybackPageState extends State<VideoPlaybackPage> {
   void dispose() {
     _controller.dispose();
     _chewieController.dispose();
+    _speech.stop();
+    _audioPlayer.dispose();
     super.dispose();
   }
 
-  void _listen() async {
+  Future<void> _startListening() async {
+    _recognizedText = '';
     if (!_isListening) {
       bool available = await _speech.initialize(
-        onStatus: (val) => print('onStatus: $val'),
-        onError: (val) => print('onError: $val'),
+        onStatus: (val) {
+          if (val == 'doneListening') {
+            setState(() {
+              _isListening = false;
+            });
+          }
+        },
+        onError: (val) {
+          print('onError: $val');
+          setState(() {
+            _isListening = false;
+          });
+        },
       );
       if (available) {
         setState(() => _isListening = true);
         _speech.listen(
           listenOptions: stt.SpeechListenOptions(
-            partialResults: true,
+            partialResults: false,
           ),
-          onResult: (val) => setState(() {
-            _recognizedText = val.recognizedWords;
-            double similarity = _calculateSimilarity();
-            int grade = (similarity * 100).toInt();
-            if (similarity >= 0.5) {
-              // Update the database to mark the exercise as completed
-              _updateDatabase(true, grade);
-              _showCelebrationAnimation();
-              Navigator.of(context).pop();
-            } else {
-              _updateDatabase(false, grade);
-            }
-          }),
+          onResult: (val) {
+            setState(() {
+              _recognizedText = val.recognizedWords;
+            });
+          },
           localeId: 'en_US',
         );
       }
-    } else {
+    }
+  }
+
+  Future<void> _stopListening() async {
+    if (_isListening) {
       setState(() => _isListening = false);
       _speech.stop();
     }
+    if (_recognizedText.isNotEmpty) {
+      await _evaluateSpeech(_recognizedText);
+    }
+  }
+
+  Future<void> _evaluateSpeech(String recognizedText) async {
+    String recognizedTextLower = recognizedText.toLowerCase();
+    String videoTitleLower = widget.videoTitle.toLowerCase();
+
+    String g2pExpected = videoTitleLower;
+    String g2pRecognized = recognizedTextLower;
+
+    if (_useIPA) {
+      // G2P conversion
+      g2pExpected = await G2PAPI().getIPA(videoTitleLower);
+      g2pRecognized = await G2PAPI().getIPA(recognizedTextLower);
+
+      // Use the original text if G2P conversion fails
+      if (g2pExpected.isEmpty) g2pExpected = videoTitleLower;
+      if (g2pRecognized.isEmpty) g2pRecognized = recognizedTextLower;
+    }
+
+    print('Expected: $g2pExpected');
+    print('Recognized: $g2pRecognized');
+    double similarity = _calculateSimilarity(g2pExpected, g2pRecognized);
+    aboveSimilarityThreshhold = similarity >= 0.80;
+    int grade = (similarity * 100).toInt();
+
+    if (true) {
+      _updateDatabase(true, grade);
+      _showCelebrationAnimation();
+      // Use Gemini and Text-to-Speech APIs
+      String encouragement = await GeminiAPI().getEncouragement(
+          "Your Job Is To Encourage A Child That Is Doing Speech Therapy Exercises,The Exercises Are Presented To The Child As Part Of A Game Once He Does This Exercise Close Enough He Can Continue To The Next Level, don't give too specific advice, Therapist Said: $videoTitleLower, Child Said: $recognizedTextLower, Grade By Therapist: $grade%");
+      String audioContent =
+          await TextToSpeechAPI().getSpeechAudio(encouragement);
+      _playAudio(audioContent);
+    } else {
+      _updateDatabase(false, grade);
+    }
+  }
+
+  Future<void> _playAudio(String audioContent) async {
+    try {
+      final bytes = base64Decode(audioContent);
+      await _audioPlayer.play(BytesSource(bytes));
+    } catch (e) {
+      _showErrorDialog('Error playing audio: $e');
+    }
+  }
+
+  double _calculateSimilarity(String s1, String s2) {
+    int editDistance = _calculateEditDistance(s1, s2);
+    double maxLen =
+        s1.length > s2.length ? s1.length.toDouble() : s2.length.toDouble();
+    double similarity = 1.0 - (editDistance / maxLen);
+    print(similarity.toStringAsFixed(2));
+    return similarity;
+  }
+
+  int _calculateEditDistance(String s1, String s2) {
+    List<List<int>> dp =
+        List.generate(s1.length + 1, (_) => List<int>.filled(s2.length + 1, 0));
+
+    for (int i = 0; i <= s1.length; i++) {
+      for (int j = 0; j <= s2.length; j++) {
+        if (i == 0) {
+          dp[i][j] = j;
+        } else if (j == 0) {
+          dp[i][j] = i;
+        } else if (s1[i - 1] == s2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else {
+          dp[i][j] = 1 + _min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
+        }
+      }
+    }
+
+    return dp[s1.length][s2.length];
+  }
+
+  int _min(int a, int b, int c) {
+    return a < b ? (a < c ? a : c) : (b < c ? b : c);
   }
 
   void _updateDatabase(bool completed, int grade) {
@@ -153,15 +253,6 @@ class _VideoPlaybackPageState extends State<VideoPlaybackPage> {
     databaseReference.update({
       'status': completed ? 'childAttempted' : 'childDidNotAttempt',
       'grade': grade,
-    }).then((_) {
-      Fluttertoast.showToast(
-          msg: "Database updated successfully",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.BOTTOM,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.green,
-          textColor: Colors.white,
-          fontSize: 16.0);
     }).catchError((error) {
       Fluttertoast.showToast(
           msg: "Failed to update database: $error",
@@ -205,51 +296,21 @@ class _VideoPlaybackPageState extends State<VideoPlaybackPage> {
     });
   }
 
-  double _calculateSimilarity() {
-    String recognizedTextLower = _recognizedText.toLowerCase();
-    String videoTitleLower = widget.videoTitle.toLowerCase();
-
-    int editDistance =
-        _calculateEditDistance(recognizedTextLower, videoTitleLower);
-    double maxLen = recognizedTextLower.length > videoTitleLower.length
-        ? recognizedTextLower.length.toDouble()
-        : videoTitleLower.length.toDouble();
-
-    double similarity = 1.0 - (editDistance / maxLen);
-    print(similarity.toStringAsFixed(2));
-    return similarity;
-  }
-
-  int _calculateEditDistance(String s1, String s2) {
-    List<List<int>> dp =
-        List.generate(s1.length + 1, (_) => List<int>.filled(s2.length + 1, 0));
-
-    for (int i = 0; i <= s1.length; i++) {
-      for (int j = 0; j <= s2.length; j++) {
-        if (i == 0) {
-          dp[i][j] = j;
-        } else if (j == 0) {
-          dp[i][j] = i;
-        } else if (s1[i - 1] == s2[j - 1]) {
-          dp[i][j] = dp[i - 1][j - 1];
-        } else {
-          dp[i][j] = 1 + _min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
-        }
-      }
-    }
-
-    return dp[s1.length][s2.length];
-  }
-
-  int _min(int a, int b, int c) {
-    return a < b ? (a < c ? a : c) : (b < c ? b : c);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.videoTitle),
+        actions: [
+          Switch(
+            value: _useIPA,
+            onChanged: (value) {
+              setState(() {
+                _useIPA = value;
+              });
+            },
+          ),
+        ],
       ),
       body: Stack(
         children: [
@@ -268,9 +329,15 @@ class _VideoPlaybackPageState extends State<VideoPlaybackPage> {
                         style: TextStyle(
                             fontSize: 24, fontWeight: FontWeight.bold),
                       ),
-                      FloatingActionButton(
-                        onPressed: _listen,
-                        child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+                      GestureDetector(
+                        onTapDown: (_) => _startListening(),
+                        onTapUp: (_) => _stopListening(),
+                        child: FloatingActionButton(
+                          onPressed:
+                              () {}, // Required parameter, not used here.
+                          child:
+                              Icon(_isListening ? Icons.mic : Icons.mic_none),
+                        ),
                       ),
                       if (_recognizedText.isNotEmpty)
                         Padding(
@@ -286,7 +353,7 @@ class _VideoPlaybackPageState extends State<VideoPlaybackPage> {
                               Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  _calculateSimilarity() >= 0.5
+                                  aboveSimilarityThreshhold
                                       ? const Icon(Icons.check,
                                           color: Colors.green, size: 30)
                                       : const Icon(Icons.close,
